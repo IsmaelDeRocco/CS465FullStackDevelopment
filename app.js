7173a5b40fe4d0de74f84d36c24674240c95c759
Ismael DeRocco <ismael.derocco@snhu.edu>
2023-01-15 10:11:30 -0500

    initial common

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 var hbs = require('hbs');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 
 
 // register handlebars partials(https://www.npmjs.com/package/hbs)
