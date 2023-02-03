a96311537eb0757a3084fb9a16ce63258e67e754
Ismael DeRocco <ismael.derocco@snhu.edu>
2023-02-02 19:07:11 -0500

    Create new application API

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const model = mongoose.model('trips');
+
+// GET: /trips - list all the trips
+const tripList = async (req, res) => {
+    model
+        .find({}) //empty filter for all 
+        .exec((err, trips) => {
+            if(!trips){
+                return res
+                .status(404)
+                .json({"message":"trip not found"});
+            } else if (err){
+                return res
+                .status(404)
+                .json(err);
+            }else{
+                return res
+                .status(200)
+                .json(trips);
+            }
+        });
+
+};
+
+const tripsFindByCode = async (req, res) => {
+    model
+        .find({ 'code': req.params.tripCode })
+        .exec((err, trip) => {
+            if(!trip) {
+                return res
+                .status(404)
+                .json({"message":"trip not found"})
+            }else if(err){
+                return res
+                .status(404)
+                .json(err);
+            }else{
+                return res
+                .status(200)
+                .json(trips);
+            }
+        });
+};
+
+
+module.exports = {
+    tripList,
+    tripsFindByCode
+
+};
