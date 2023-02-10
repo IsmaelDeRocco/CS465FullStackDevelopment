3b08ce9282dd56025000bea03cba3b51e3541c82
Ismael DeRocco <ismael.derocco@snhu.edu>
2023-02-10 16:05:08 -0500

    Module 6

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -8,11 +8,27 @@ export class TripDataService{
   constructor(private http: Http) { }
 
   private apiBaseURL = 'http://localhost:3000/api/';
+private tripUrl = `${this.apiBaseURL}trips/`
 
+
+
+public addTrip(formData: Trip): Promise<Trip> {
+  console.log('Inside TripDataService#addTrips');
+  return this.http
+  .post(this.tripUrl, formData)
+  .toPromise()
+  .then(response => response.json() as Trip[])
+  .catch(this.handleError);
+}
+  
+  
+  
+  
+  
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     return this.http
-      .get(`${this.apiBaseURL}trips`)
+      .get(this.tripUrl)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
