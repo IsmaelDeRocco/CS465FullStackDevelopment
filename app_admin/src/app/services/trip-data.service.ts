10b0c528d01ba4e4cfdb86c23e5b6728ecaad88b
Ismael DeRocco <ismael.derocco@snhu.edu>
2023-02-11 13:08:40 -0500

    Module 6

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -4,42 +4,51 @@ import { Http } from '@angular/http';
 import { Trip } from '../models/trip';
 
 @Injectable()
-export class TripDataService{
+export class TripDataService {
+
   constructor(private http: Http) { }
 
-  private apiBaseURL = 'http://localhost:3000/api/';
-private tripUrl = `${this.apiBaseURL}trips/`
+  private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripUrl = `${this.apiBaseUrl}trips/`;
 
-
-
-public addTrip(formData: Trip): Promise<Trip> {
-  console.log('Inside TripDataService#addTrips');
-  return this.http
-  .post(this.tripUrl, formData)
-  .toPromise()
-  .then(response => response.json() as Trip[])
-  .catch(this.handleError);
-}
-  
-  
-  
-  
-  
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     return this.http
-      .get(this.tripUrl)
+      .get(`${this.apiBaseUrl}trips`)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
-  
-    }
-
-  private handleError(error: any): Promise<any> {
-    console.error('Something has gone wrong', error); //for demo purposes only
-    return Promise.reject(error.message || error);
-
   }
 
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getdTrip');
+    return this.http
+      .get(this.tripUrl + tripCode)
+      .toPromise()
+      .then(response => response.json() as Trip)
+      .catch(this.handleError);
+  }
 
+  public addTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#addTrip');
+    return this.http
+      .post(this.tripUrl, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#updateTrip');
+    return this.http
+      .put(this.tripUrl + formData.code, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Something has gone wrong', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
