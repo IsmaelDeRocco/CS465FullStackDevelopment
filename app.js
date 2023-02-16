6a8c6d41ece757ec986f6ee18ae160a33d40d8c3
Ismael DeRocco <ismael.derocco@snhu.edu>
2023-02-16 08:34:21 -0500

    Module 7

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const passport = require('passport');
 var hbs = require('hbs');
 require('./app_api/models/db');
 
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -58,4 +58,4 @@ connect();
 
 //bring in the Mongoose schema
 require('./travlr');
-require('./user');
+require('../models/user');
